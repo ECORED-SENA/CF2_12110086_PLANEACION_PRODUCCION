52241622c9478c0956b22835bbc8a8ebce5065fd
SANDRATRIANA <sandrajem23@hotmail.com>
2022-09-23 17:39:58 -0500

    video

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -712,8 +712,8 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-          
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/aWe16wi-uk4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            
         
 
       
